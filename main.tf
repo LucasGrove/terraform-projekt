46cdf99b98719eda09e191ae843424cf9a79765c
Lucas Castor Grove Schmidt <luca4355@plato.local>
2026-04-14 14:47:18 +0200

    update project

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+} 
+
+resource "docker_container" "web" {
+  name  = "my-nginx"
+  image = docker_image.nginx.image_id
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
